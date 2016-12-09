bcc7068c4318b78b01e50c4c02bd7dbc35408237
nailyk-fr <nailyk_git@nailyk.fr>
2016-12-09 19:41:44 +0100

    Build: z3: Add repo clean steps

--- z3.sh
+++ z3.sh
@@ -5,21 +5,29 @@ BUILD_VER="cm-12.1"
 echo "---------home set to: $HOME"
 
 cd ~/vendor/cm
+git reset --hard
+git clean -fxd :/
 git checkout "github/${BUILD_VER}"
 sed -i 's,UNOFFICIAL,nailyk,g' config/common.mk
 echo "---------vendor cm patched"
 
 cd ~/packages/apps/Browser
+git reset --hard
+git clean -fxd :/
 git checkout "github/${BUILD_VER}"
 git fetch https://review.cyanogenmod.org/CyanogenMod/android_packages_apps_Browser refs/changes/54/163254/1 && git cherry-pick FETCH_HEAD
 echo "---------package apps Browser patched"
 
 cd ~/frameworks/base
+git reset --hard
+git clean -fxd :/
 git checkout "github/${BUILD_VER}"
 git fetch https://review.cyanogenmod.org/CyanogenMod/android_frameworks_base refs/changes/13/157413/2 && git cherry-pick FETCH_HEAD
 echo "---------frameworks base patched"
 
 cd ~/device/sony/z3
+git reset --hard
+git clean -fxd :/
 git checkout "github/${BUILD_VER}"
 git fetch https://review.cyanogenmod.org/CyanogenMod/android_device_sony_z3 refs/changes/41/159441/1 && git cherry-pick FETCH_HEAD
 echo "---------device sony z3 patched"

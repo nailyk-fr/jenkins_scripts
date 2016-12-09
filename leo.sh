f4425d0d2a7ca16f967eb8b8ffd697b33462557b
nailyk-fr <nailyk_git@nailyk.fr>
2016-12-09 19:40:00 +0100

    Build: leo: add clean repo step

--- leo.sh
+++ leo.sh
@@ -3,6 +3,8 @@
 echo "---------home set to: $HOME"
 
 cd ~/vendor/cm
+git reset --hard
+git clean -fxd :/
 git checkout github/cm-14.1
 git fetch https://review.cyanogenmod.org/CyanogenMod/android_vendor_cm refs/changes/49/175749/5 && git cherry-pick FETCH_HEAD
 sed -i 's,UNOFFICIAL,nailyk,g' config/common.mk

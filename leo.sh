6398979172c3ead7b97f14266a49f6dd7b8a7ac7
nailyk-fr <nailyk_git@nailyk.fr>
2016-12-09 19:13:31 +0100

    Build: add leo build script

---
+++ leo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "---------home set to: $HOME"
+
+cd ~/vendor/cm
+git checkout github/cm-14.1
+git fetch https://review.cyanogenmod.org/CyanogenMod/android_vendor_cm refs/changes/49/175749/5 && git cherry-pick FETCH_HEAD
+sed -i 's,UNOFFICIAL,nailyk,g' config/common.mk
+
+cd ~/
+source ./build/envsetup.sh
+breakfast leo
+make toto -j4
+

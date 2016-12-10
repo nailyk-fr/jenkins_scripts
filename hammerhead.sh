263b532cbe3a7a0191eb6569c5f60c332f388726
nailyk-fr <nailyk_git@nailyk.fr>
2016-12-10 02:00:54 +0100

    Hammerhead: Patch: add sepolicy workaround to allow Copperhead build

--- hammerhead.sh
+++ hammerhead.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BUILD_VER=""
+BUILD_VER="nougat-mr0.5-release"
 
 echo "---------home set to: $HOME"
 
@@ -11,5 +11,12 @@ unset _JAVA_OPTIONS
 export BUILD_NUMBER=$(date --utc +%Y.%m.%d.%H.%M.%S)
 export DISPLAY_BUILD_NUMBER=true
 
+cd ~/system/sepolicy
+git reset --hard
+git clean -fxd :/
+git checkout "github/${BUILD_VER}"
+patch -p1 < ~/device/lge/hammerhead/patches/system_sepolicy.patch
+
+cd ~/
 lunch aosp_hammerhead-userdebug
 make bootimage -j4

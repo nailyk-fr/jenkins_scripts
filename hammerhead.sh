77a3ccce4e4a89a4caba77b1eb3cbe37ef248614
nailyk-fr <nailyk_git@nailyk.fr>
2016-12-10 01:55:18 +0100

    Hammerhead: change script according to CopperHead build inscructions

--- hammerhead.sh
+++ hammerhead.sh
@@ -6,5 +6,10 @@ echo "---------home set to: $HOME"
 
 cd ~/
 source ./build/envsetup.sh
+export LANG=C
+unset _JAVA_OPTIONS
+export BUILD_NUMBER=$(date --utc +%Y.%m.%d.%H.%M.%S)
+export DISPLAY_BUILD_NUMBER=true
+
 lunch aosp_hammerhead-userdebug
 make bootimage -j4

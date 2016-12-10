41d6a3998f3361db800ca70e54a5bed2edd4187a
nailyk-fr <nailyk_git@nailyk.fr>
2016-12-10 18:32:36 +0100

    Hammerhead add jack vm parameters

--- hammerhead.sh
+++ hammerhead.sh
@@ -18,11 +18,26 @@ git clean -fxd :/
 git checkout "github/${BUILD_VER}"
 patch -p1 < ~/device/lge/hammerhead/patches/system_sepolicy.patch
 
+echo "--------sepolicyes patched"
+
 cd ~/kernel/lge/hammerhead
 git reset --hard
 git clean -fxd :/
 git checkout "github/cm-14.1"
 patch -p1 < ~/device/lge/hammerhead/patches/kernel.diff
+
+
+echo "--------kernel patched"
+
+
+export JACK_SERVER_VM_ARGUMENTS=-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4g
+export ANDROID_JACK_VM_ARGS=$JACK_SERVER_VM_ARGUMENTS
+
+./prebuilts/sdk/tools/jack-admin kill-server
+./prebuilts/sdk/tools/jack-admin start-server
+
+echo "--------jack restarted with $ANDROID_JACK_VM_ARGS"
+
 
 cd ~/
 lunch aosp_hammerhead-userdebug

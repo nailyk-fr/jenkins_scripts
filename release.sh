735239fb45c0e0679f7f6e9b7b40a53184243861
nailyk-fr <nailyk_git@nailyk.fr>
2016-12-22 21:45:47 +0100

    common: add release script

---
+++ release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+echo publish for $1
+
+if [ ! -d "$HOME/../releases/cm-14.1" ]; then 
+	mkdir $HOME/../releases/cm-14.1
+fi
+
+if [ -d "$HOME/../releases/cm-14.1" ]; then 
+	cp $HOME/out/target/product/$1/cm-*.zip* $HOME/../releases/cm-14.1
+else
+	false
+fi

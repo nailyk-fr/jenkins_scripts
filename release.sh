b8ce36912e861fc00efeae53d3ebf03d3b876fd0
nailyk-fr <nailyk_git@nailyk.fr>
2016-12-22 21:55:30 +0100

    release: correct build number

--- release.sh
+++ release.sh
@@ -3,12 +3,12 @@
 
 echo publish for $1
 
-if [ ! -d "$HOME/../releases/cm-14.1" ]; then 
-	mkdir $HOME/../releases/cm-14.1
+if [ ! -d "$HOME/../releases/cm-12.1" ]; then 
+	mkdir $HOME/../releases/cm-12.1
 fi
 
-if [ -d "$HOME/../releases/cm-14.1" ]; then 
-	cp $HOME/out/target/product/$1/cm-*.zip* $HOME/../releases/cm-14.1
+if [ -d "$HOME/../releases/cm-12.1" ]; then 
+	cp $HOME/out/target/product/$1/cm-*.zip* $HOME/../releases/cm-12.1
 else
 	false
 fi

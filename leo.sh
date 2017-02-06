dd3b6c4dd016b981caf0f34868e68a35214f6189
nailyk-fr <nailyk_git@nailyk.fr>
2017-02-06 20:52:58 +0100

    Revert "leo: disable "add usb workaround""
    This reverts commit 85532efde640938b1371930fa30ac0226e2c60ec.

--- leo.sh
+++ leo.sh
@@ -17,7 +17,7 @@ cd ~/kernel/sony/msm8974
 git reset --hard
 git clean -fxd :/
 git checkout github/aosp/LA.BR.1.3.3_rb2.14
-#patch -p1 < ~/scripts/patches/usb_workaround.patch
+patch -p1 < ~/scripts/patches/usb_workaround.patch
 
 cd ~/hardware/qcom/display
 git reset --hard

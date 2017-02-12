8c1c149aaf98d526f2f1eafce14fa5679d3b4b43
nailyk-fr <nailyk_git@nailyk.fr>
2017-02-12 20:09:53 +0100

    z3c: Add aries/z3c

---
+++ z3c.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+BUILD_VER="cm-12.1"
+export USER=jenkins
+export LANG=C
+export JAVA_HOME="/usr/lib/jvm/java-7-openjdk-amd64/"
+
+echo "---------home set to: $HOME"
+
+cd ~/vendor/cm
+git reset --hard
+git clean -fxd :/
+git checkout "github/${BUILD_VER}"
+sed -i 's,UNOFFICIAL,nailyk,g' config/common.mk
+echo "---------vendor cm patched"
+
+cd ~/packages/apps/Browser
+git reset --hard
+git clean -fxd :/
+git checkout "github/${BUILD_VER}"
+#git fetch https://review.lineageos.org/LineageOS/android_packages_apps_Browser refs/changes/52/110752/1 && git cherry-pick FETCH_HEAD
+echo "---------package apps Browser patched"
+
+cd ~/frameworks/base
+git reset --hard
+git clean -fxd :/
+git checkout "github/${BUILD_VER}"
+#git fetch https://review.lineageos.org/LineageOS/android_frameworks_base refs/changes/64/65064/2 && git cherry-pick FETCH_HEAD
+echo "---------frameworks base patched"
+
+cd ~/device/sony/z3
+git reset --hard
+git clean -fxd :/
+git checkout "github/${BUILD_VER}"
+#git fetch https://review.lineageos.org/LineageOS/android_device_sony_z3 refs/changes/65/56765/1 && git cherry-pick FETCH_HEAD
+patch -p1 < ~/scripts/patches/z3c_defconfig.diff
+echo "---------device sony z3c patched"
+
+
+cd ~/
+source ./build/envsetup.sh
+breakfast z3
+make bacon -j6
+

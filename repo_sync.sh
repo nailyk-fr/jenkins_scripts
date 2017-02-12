5658b4bdb6e3c045a9c901424a3d70d14b07af7a
nailyk-fr <nailyk_git@nailyk.fr>
2017-02-12 13:49:09 +0100

    z3: Use current branches
     * Since the swith to LineageOS new branch are used

--- repo_sync.sh
+++ repo_sync.sh
@@ -2,11 +2,11 @@ BUILD_VER="cm-14.1"
 
 cd ~/.repo
 if [ ! -d "$HOME/.repo/local_manifests" ]; then
-  git clone https://github.com/nailyk-fr/local_manifests.git -b "${BUILD_VER}-ADC_work" local_manifests
+  git clone https://github.com/nailyk-fr/local_manifests.git -b "${BUILD_VER}-ADC_z317" local_manifests
 else
   cd ~/.repo/local_manifests
-  git fetch origin "${BUILD_VER}-ADC_work"
-  git checkout "origin/${BUILD_VER}-ADC_work"
+  git fetch origin "${BUILD_VER}-ADC_z317"
+  git checkout "origin/${BUILD_VER}-ADC_z317"
 fi
 
 cd ~

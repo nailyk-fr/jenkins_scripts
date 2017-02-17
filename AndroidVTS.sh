7c835f638f28730635e91b801318fa6353cca4a6
nailyk-fr <nailyk_git@nailyk.fr>
2017-02-17 23:48:36 +0100

    VTS: Allow AndroidVTS build in chain
     * Create AndroidVTS.mk file
     * Create sh file to build from jenkins

---
+++ AndroidVTS.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+BUILD_VER="nougat-mr1.1-release"
+export USER=jenkins
+export GRADLE_USER_HOME=$HOME
+echo "---------home set to: $HOME"
+
+export ANDROID_HOME=/var/jenkins_home/SDK
+/var/jenkins_home/SDK/tools/bin/sdkmanager --update
+echo "---------end of sdk update $ANDROID_HOME"
+
+cd ~/
+source ./build/envsetup.sh
+export LANG=C
+unset _JAVA_OPTIONS
+export BUILD_NUMBER=$(date --utc +%Y.%m.%d.%H.%M.%S)
+export DISPLAY_BUILD_NUMBER=true
+
+cd ~/packages/apps/AndroidVTS/
+git reset --hard
+git clean -fxd :/
+git checkout "github/master"
+cp ~/patches/AndroidVTS.mk ~/packages/apps/AndroidVTS/Android.mk
+
+echo "--------AndroidVTS patched"
+
+export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4g"
+export ANDROID_JACK_VM_ARGS="$JACK_SERVER_VM_ARGUMENTS"
+
+
+cd ~/
+lunch aosp_hammerhead-userdebug
+make AndroidVTS
+
+cp ~/out/target/product/hammerhead/system/app/AndroidVTS/AndroidVTS.apk /var/jenkins_home/workspace/releases/AndroidVTS/AndroidVTS_$(date +%Y-%m-%d.%H:%M:%S)_signed.apk
---
+++ patches/AndroidVTS.mk
@@ -0,0 +1,31 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := AndroidVTS
+LOCAL_MODULE_TAGS := optional
+LOCAL_PACKAGE_NAME := AndroidVTS
+
+vts_root  := $(LOCAL_PATH)
+vts_dir   := app 
+vts_out   := $(PWD)/$(OUT_DIR)/target/common/obj/APPS/$(LOCAL_MODULE)_intermediates
+vts_build := $(vts_root)/$(vts_dir)/build
+vts_apk   := build/outputs/apk/$(vts_dir)-release-unsigned.apk
+
+$(vts_root)/$(vts_dir)/$(vts_apk):
+	rm -Rf $(vts_build)
+	mkdir -p $(vts_out)
+	ln -sf $(vts_out) $(vts_build)
+	cd $(vts_root) && \
+	APP_PROJECT_PATH=${HOME}/packages/apps/AndroidVTS/app/src/main \
+	NDK_PROJECT_PATH=${HOME}/packages/apps/AndroidVTS/app/src/main \
+	APP_BUILD_SCRIPT=${HOME}/packages/apps/AndroidVTS/app/src/main/jni/Android.mk \
+	./gradlew assembleRelease
+
+LOCAL_CERTIFICATE := platform
+LOCAL_SRC_FILES := $(vts_dir)/$(vts_apk)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+
+include $(BUILD_PREBUILT)
+

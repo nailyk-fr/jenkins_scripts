b89be80f2a8b2258afa35d3aa369618d380e7801
nailyk-fr <nailyk_git@nailyk.fr>
2017-03-02 13:21:47 +0100

    sync: Use correct repo

--- repo_sync.sh
+++ repo_sync.sh
@@ -3,7 +3,7 @@ LOCAL_BRANCH="${BUILD_VER}_stock"
 
 cd ~/.repo
 if [ ! -d "$HOME/.repo/local_manifests" ]; then
-  git clone https://github.com/nailyk-fr/local_manifests.git -b ${LOCAL_BRANCH} local_manifests
+  git clone https://github.com/nailyk-fr/android_manifest.git -b ${LOCAL_BRANCH} local_manifests
 else
   cd ~/.repo/local_manifests
   git fetch origin ${LOCAL_BRANCH}

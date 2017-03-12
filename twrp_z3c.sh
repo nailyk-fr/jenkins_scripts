744e24b2028ab84d3b4a809a04e525a3a7844845
nailyk-fr <nailyk_git@nailyk.fr>
2017-03-12 19:57:15 +0100

    all: Don't fail on useles commands

--- twrp_z3c.sh
+++ twrp_z3c.sh
@@ -13,7 +13,7 @@ cd ~/
 export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4g"
 export ANDROID_JACK_VM_ARGS="$JACK_SERVER_VM_ARGUMENTS"
 
-./prebuilts/sdk/tools/jack-admin kill-server
+./prebuilts/sdk/tools/jack-admin kill-server || true
 
 echo "--------jack args $ANDROID_JACK_VM_ARGS"
 echo "-----Trigger build from $PWD"
@@ -22,4 +22,4 @@ breakfast z3c
 make recovery-zip -j6
 
 #kill jack when done to prevent error on other builds
-./prebuilts/sdk/tools/jack-admin kill-server
+./prebuilts/sdk/tools/jack-admin kill-server || true

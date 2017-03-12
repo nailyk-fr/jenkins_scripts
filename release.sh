c32cd5a7eb35839a497e94376563386651b480bc
nailyk-fr <nailyk_git@nailyk.fr>
2017-03-12 16:30:46 +0100

    release: Add twrp releases

--- release.sh
+++ release.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
 
-echo publish for $1
-
 if [ ! -d "$HOME/../releases/cm-14.1" ]; then 
-	mkdir $HOME/../releases/cm-14.1
+	mkdir -p $HOME/../releases/cm-14.1
 fi
 
 if [ -d "$HOME/../releases/cm-14.1" ]; then 
-	find $HOME/out/target/product -maxdepth 2 -mindepth 2 -iname "lineage-*.zip" -exec cp {} $HOME/../releases/cm-14.1 \;
+	echo copy lineage
+	find $HOME/out/target/product -maxdepth 2 -mindepth 2 -iname "lineage-*.zip" -print -exec cp {} $HOME/../releases/cm-14.1 \;
 else
 	false
 fi
+
+if [ ! -d "$HOME/../releases/twrp" ]; then
+	mkdir -p $HOME/../releases/twrp
+fi
+
+if [ -d "$HOME/../releases/twrp" ]; then
+	for folder in `find $HOME/out/target/product -maxdepth 1 -type d`; do
+		echo copy twrp
+		filename="twrp_$(basename $folder)_$(date +"%Y-%m-%d")"
+		if [ -e "$folder/recovery.img" ]; then
+			eval cp -v $folder/recovery.img $HOME/../releases/twrp/$filename.img
+		fi
+		if [ -e "$folder/recovery-updater-signed.zip" ]; then
+			eval cp -v $folder/recovery-updater-signed.zip $HOME/../releases/twrp/$filename.zip
+		fi
+	done
+else
+	false
+fi
+

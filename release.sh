c59aa8433c683583ed415c444e5f2e4f7bc88a0b
nailyk <nailyk-fr@users.noreply.github.com>
2017-03-13 09:54:51 +0100

    releases: Do not override existing files

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@ fi
 
 if [ -d "$HOME/../releases/cm-14.1" ]; then 
 	echo copy lineage
-	find $HOME/out/target/product -maxdepth 2 -mindepth 2 -iname "lineage-*.zip*" -print -exec cp {} $HOME/../releases/cm-14.1 \;
+	find $HOME/out/target/product -maxdepth 2 -mindepth 2 -iname "lineage-*.zip*" -print -exec cp --no-clobber {} $HOME/../releases/cm-14.1 \;
 else
 	false
 fi
@@ -21,10 +21,10 @@ if [ -d "$HOME/../releases/twrp" ]; then
 		echo copy twrp
 		filename="twrp_$(basename $folder)_$(date +"%Y-%m-%d")"
 		if [ -e "$folder/recovery.img" ]; then
-			eval cp -v $folder/recovery.img $HOME/../releases/twrp/$filename.img
+			eval cp -v --no-clobber $folder/recovery.img $HOME/../releases/twrp/$filename.img
 		fi
 		if [ -e "$folder/recovery-updater-signed.zip" ]; then
-			eval cp -v $folder/recovery-updater-signed.zip $HOME/../releases/twrp/$filename.zip
+			eval cp -v --no-clobber $folder/recovery-updater-signed.zip $HOME/../releases/twrp/$filename.zip
 		fi
 	done
 else

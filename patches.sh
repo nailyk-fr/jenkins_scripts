123ee1020e269e336107eb99e4769dbf4f3afc8e
nailyk <nailyk-fr@users.noreply.github.com>
2017-03-14 22:01:24 +0100

    patch: Add twrp terminal patch

--- patches.sh
+++ patches.sh
@@ -15,6 +15,7 @@ sed -i 's,UNOFFICIAL,nailyk,g' config/common.mk
 cd ~/bootable/recovery
 echo "-----Patching $PWD"
 git fetch https://gerrit.omnirom.org/android_bootable_recovery refs/changes/96/22096/3 && git cherry-pick FETCH_HEAD
+git fetch https://gerrit.omnirom.org/android_bootable_recovery refs/changes/69/22369/1 && git cherry-pick FETCH_HEAD
 
 cd ~/
 

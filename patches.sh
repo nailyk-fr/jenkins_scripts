0f24aafa3aeec89fcc8fa7462f355808de2e5f07
nailyk-fr <nailyk_git@nailyk.fr>
2017-03-28 22:46:58 +0200

    patchs: Add pending kernel patches
    Change-Id: Ib75bc08cd0be7beeaa6e70317f751900a5f973c1

--- patches.sh
+++ patches.sh
@@ -29,9 +29,15 @@ repopick -g https://gerrit.nailyk.fr -P device/sony/shinano-common 20
 repopick -g https://gerrit.nailyk.fr -P device/sony/shinano-common 21
 repopick -g https://gerrit.nailyk.fr -P device/sony/shinano-common 22
 repopick -g https://gerrit.nailyk.fr -P device/sony/shinano-common 28
+repopick -g https://gerrit.nailyk.fr -P device/sony/shinano-common 69
 
 repopick -g https://gerrit.nailyk.fr -P device/sony/z3 17
 repopick -g https://gerrit.nailyk.fr -P device/sony/z3 26
 repopick -g https://gerrit.nailyk.fr -P device/sony/z3c 16
 repopick -g https://gerrit.nailyk.fr -P device/sony/z3c 27
 
+repopick -g https://gerrit.nailyk.fr -P kernel/sony/msm8974 29
+for i in {47..68}
+do
+  repopick -g https://gerrit.nailyk.fr -P kernel/sony/msm8974 $i
+done

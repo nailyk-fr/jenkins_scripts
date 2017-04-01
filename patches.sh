7bd295b20e8e4256a6bcce74a01eb43cb1d0ec5c
nailyk <nailyk-fr@users.noreply.github.com>
2017-04-01 02:31:10 +0200

    patchs: Add pending commits

--- patches.sh
+++ patches.sh
@@ -22,6 +22,9 @@ source ./build/envsetup.sh
 echo "-----Patching vendor"
 repopick -g https://gerrit.nailyk.fr -P vendor/sony 72
 
+echo "-----Patching device/sony/msm8974-common"
+repopick -g https://gerrit.nailyk.fr -P device/sony/shinano-common 79
+
 echo "-----Patching device/sony/shinano-common"
 repopick -g https://gerrit.nailyk.fr -P device/sony/shinano-common 18
 repopick -g https://gerrit.nailyk.fr -P device/sony/shinano-common 19
@@ -31,6 +34,7 @@ repopick -g https://gerrit.nailyk.fr -P device/sony/shinano-common 22
 repopick -g https://gerrit.nailyk.fr -P device/sony/shinano-common 28
 repopick -g https://gerrit.nailyk.fr -P device/sony/shinano-common 69
 repopick -g https://gerrit.nailyk.fr -P device/sony/shinano-common 70
+repopick -g https://gerrit.nailyk.fr -P device/sony/shinano-common 78
 
 echo "-----Patching device/sony/z3"
 repopick -g https://gerrit.nailyk.fr -P device/sony/z3 17
@@ -38,6 +42,7 @@ repopick -g https://gerrit.nailyk.fr -P device/sony/z3 26
 echo "-----Patching device/sony/z3c"
 repopick -g https://gerrit.nailyk.fr -P device/sony/z3c 16
 repopick -g https://gerrit.nailyk.fr -P device/sony/z3c 27
+repopick -g https://gerrit.nailyk.fr -P device/sony/z3c 80
 
 echo "-----Patching kernel/sony/msm8974"
 repopick -g https://gerrit.nailyk.fr -P kernel/sony/msm8974 13
@@ -49,8 +54,12 @@ for i in {47..68}
 do
   repopick -g https://gerrit.nailyk.fr -P kernel/sony/msm8974 $i
 done
-repopick -g https://gerrit.nailyk.fr -P kernel/sony/msm8974 73
-repopick -g https://gerrit.nailyk.fr -P kernel/sony/msm8974 74
+for i in {73..77}
+do
+  repopick -g https://gerrit.nailyk.fr -P kernel/sony/msm8974 $i
+done
+
+
 
 # Pending nfc patch
 repopick 161917

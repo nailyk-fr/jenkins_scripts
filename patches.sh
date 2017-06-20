3012f82fd8dfae16b96eb59355a18f0ece7162c5
nailyk-fr <nailyk_git@nailyk.fr>
2017-06-20 21:31:26 +0200

    z3dual: Add build scripts
    Change-Id: I03c1c95ff6c69bb059f38d4d31fd7dd3e05f0de1

--- patches.sh
+++ patches.sh
@@ -66,6 +66,17 @@ git checkout github/cm-14.1
 cd ~/
 repopick -Q "status:open+project:android_kernel_sony_msm8974"  -g https://gerrit.nailyk.fr -P kernel/sony/msm8974 --exclude "558"
 
+echo "-----Patching device/sony/z3dual"
+cd ~/device/sony/z3dual
+git fetch github cm-14.1
+git fetch github cm-14.1_twrp
+git checkout github/cm-14.1_twrp
+git merge --no-commit github/cm-14.1
+git commit --message="Merge cm-14.1" --allow-empty
+cd ~/
+repopick -Q "status:open+project:android_device_sony_z3dual"  -g https://gerrit.nailyk.fr -P device/sony/z3dual
+
+
 # Pending nfc patch
 repopick 161917
 

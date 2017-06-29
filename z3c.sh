9815e2da58d3a3c26dc58645f0b7187afb89cae1
nailyk-fr <nailyk_git@nailyk.fr>
2017-06-29 10:38:49 +0200

    Release the power

--- z3c.sh
+++ z3c.sh
@@ -10,7 +10,7 @@ echo "---------patching"
 ./${MYFOLDER}/patches.sh
 
 cd ~/
-export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4g"
+export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx8g"
 export ANDROID_JACK_VM_ARGS="$JACK_SERVER_VM_ARGUMENTS"
 
 ./prebuilts/sdk/tools/jack-admin kill-server || true

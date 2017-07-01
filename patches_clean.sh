1213405a6355759d4cc9a6c3fede45e12dd19789
nailyk-fr <nailyk_git@nailyk.fr>
2017-07-01 23:09:29 +0200

    Clean: Colorize
    Change-Id: I62178655e738c186f7b9a5b0dcae863a85dec66d

--- patches_clean.sh
+++ patches_clean.sh
@@ -6,82 +6,89 @@ export USE_NINJA=false
 export WITH_SU=true
 MYFOLDER=$PWD/scripts
 
-echo "---------home set to: $HOME"
+#https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux#5947802
+RED='\e[0;31m'
+GREEN='\e[0;32m'
+BLUE='\e[0;34m'
+YELLOW='\e[1;33m'
+NC='\e[0m' # No Color
+
+echo -e ${GREEN}"---------home set to: $HOME"${NC}
 
 cd ~/vendor/omni
-echo "-----Cleaning $PWD"
+echo -e ${YELLOW}"-----Cleaning $PWD"${NC}
 git reset --hard
 git clean -fxd :/
 git checkout omnirom/android-7.1
 
 cd ~/vendor/sony
-echo "-----Cleaning $PWD"
+echo -e ${YELLOW}"-----Cleaning $PWD"${NC}
 git reset --hard
 git clean -fxd :/
 git checkout choosea/android-7.1
 
 cd ~/device/qcom/sepolicy
-echo "-----Cleaning $PWD"
+echo -e ${YELLOW}"-----Cleaning $PWD"${NC}
 git reset --hard
 git clean -fxd :/
 git checkout omnirom/android-7.1
 
 cd ~/system/sepolicy
-echo "-----Cleaning $PWD"
+echo -e ${YELLOW}"-----Cleaning $PWD"${NC}
 git reset --hard
 git clean -fxd :/
 git checkout omnirom/android-7.1
 
 cd ~/bootable/recovery
-echo "-----Cleaning $PWD"
+echo -e ${YELLOW}"-----Cleaning $PWD"${NC}
 git reset --hard
 git clean -fxd :/
 git checkout omnirom/android-7.1
 
 cd ~/device/sony/leo
-echo "-----Cleaning $PWD"
+echo -e ${YELLOW}"-----Cleaning $PWD"${NC}
 git reset --hard
 git clean -fxd :/
 git checkout choose/android-7.1
 
 #cd ~/device/sony/aries
-#echo "-----Cleaning $PWD"
+#echo -e ${YELLOW}"-----Cleaning $PWD"${NC}
 #git reset --hard
 #git clean -fxd :/
 #git checkout github/android-7.1
 
 #cd ~/device/sony/leodual
-#echo "-----Cleaning $PWD"
+#echo -e ${YELLOW}"-----Cleaning $PWD"${NC}
 #git reset --hard
 #git clean -fxd :/
 #git checkout github/android-7.1
 
 #cd ~/device/sony/sirius
-#echo "-----Cleaning $PWD"
+#echo -e ${YELLOW}"-----Cleaning $PWD"${NC}
 #git reset --hard
 #git clean -fxd :/
 #git checkout github/android-7.1
 
 #cd ~/device/sony/castor_windy
-#echo "-----Cleaning $PWD"
+#echo -e ${YELLOW}"-----Cleaning $PWD"${NC}
 #git reset --hard
 #git clean -fxd :/
 #git checkout github/android-7.1
 
 cd ~/device/sony/shinano-common
-echo "-----Cleaning $PWD"
+echo -e ${YELLOW}"-----Cleaning $PWD"${NC}
 git reset --hard
 git clean -fxd :/
 git checkout choose/android-7.1
 
 cd ~/device/sony/msm8974-common
-echo "-----Cleaning $PWD"
+echo -e ${YELLOW}"-----Cleaning $PWD"${NC}
 git reset --hard
 git clean -fxd :/
 git checkout choose/android-7.1
 
 cd ~/kernel/sony/msm8974
-echo "-----Cleaning $PWD"
+echo -e ${YELLOW}"-----Cleaning $PWD"${NC}
 git reset --hard
 git clean -fxd :/
 git checkout choose/android-7.1

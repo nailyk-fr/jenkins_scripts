de0e00b9e9d0a048fc38f2951c2382b48ed98124
nailyk-fr <nailyk_git@nailyk.fr>
2017-07-01 22:42:08 +0200

    Patchs: Colorize
    Change-Id: I38856c615b0d9fc43a95ae54bb7323e12dd6d1a8

--- patches.sh
+++ patches.sh
@@ -4,15 +4,22 @@ export USER=jenkins
 export LANG=C
 export USE_NINJA=false
 export WITH_SU=true
-MYFOLDER=$PWD/scripts
+MYFOLDER="$(dirname "$(realpath "$0")")"
+
+#https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux#5947802
+RED='\e[0;31m'
+GREEN='\e[0;32m'
+BLUE='\e[0;34m'
+YELLOW='\e[1;33m'
+NC='\e[0m' # No Color
 
 git config --global user.email "jenkins@nailyk.fr"
 git config --global user.name "nailyk-fr"
 
-echo "---------home set to: $HOME"
+echo -e ${GREEN}"---------home set to: $HOME"{NC}
 
 cd ~/vendor/omni
-echo "-----Patching $PWD"
+echo -e ${YELLOW}"-----Patching $PWD"${NC}
 sed -i 's,HOMEMADE,nailyk,g' config/version.mk
 sed -i 's,HOMEMADE,nailyk,g' config/common.mk
 
@@ -20,39 +27,39 @@ cd ~/
 
 source ./build/envsetup.sh
 
-echo "-----Patching device/sony/msm8974-common"
+echo -e ${YELLOW}"-----Patching device/sony/msm8974-common"${NC}
 cd ~/device/sony/msm8974-common
 git fetch choose android-7.1
 git checkout choose/android-7.1
 cd ~/
-$MYFOLDER/repopick -Q "status:open+project:android_device_sony_msm8974-common"  -g https://review.choose-a.name -P device/sony/msm8974-common
+$MYFOLDER/repopick.py -Q "status:open+project:android_device_sony_msm8974-common"  -g https://review.choose-a.name -P device/sony/msm8974-common
 
-echo "-----Patching device/sony/shinano-common"
+echo -e ${YELLOW}"-----Patching device/sony/shinano-common"${NC}
 cd ~/device/sony/shinano-common
 git fetch choose android-7.1
 git checkout choose/android-7.1
 cd ~/
-$MYFOLDER/repopick -Q "status:open+project:android_device_sony_shinano-common"  -g https://review.choose-a.name -P device/sony/shinano-common
+$MYFOLDER/repopick.py -Q "status:open+project:android_device_sony_shinano-common"  -g https://review.choose-a.name -P device/sony/shinano-common
 
-echo "-----Patching device/sony/leo"
+echo -e ${YELLOW}"-----Patching device/sony/leo"${NC}
 cd ~/device/sony/leo
 git fetch choose android-7.1
 git checkout choose/android-7.1
 cd ~/
-$MYFOLDER/repopick -Q "status:open+project:android_device_sony_leo"  -g https://review.choose-a.name -P device/sony/leo
+$MYFOLDER/repopick.py -Q "status:open+project:android_device_sony_leo"  -g https://review.choose-a.name -P device/sony/leo
 
-echo "-----Patching kernel/sony/msm8974"
+echo -e ${YELLOW}"-----Patching kernel/sony/msm8974"${NC}
 cd ~/kernel/sony/msm8974
 git fetch choose android-7.1
 git checkout choose/android-7.1
 cd ~/
-$MYFOLDER/repopick -Q "status:open+project:android_kernel_sony_msm8974"  -g https://review.choose-a.name -P kernel/sony/msm8974
+$MYFOLDER/repopick.py -Q "status:open+project:android_kernel_sony_msm8974"  -g https://review.choose-a.name -P kernel/sony/msm8974
 
-echo "-----Patching vendor"
+echo -e ${YELLOW}"-----Patching vendor"${NC}
 cd ~/vendor/sony
 git fetch choosea android-7.1
 git checkout choosea/android-7.1
 cd ~/
-$MYFOLDER/repopick -Q "status:open+project:DonkeyCoyote/proprietary_vendor_sony" -g https://review.choose-a.name -P vendor/sony
+$MYFOLDER/repopick.py -Q "status:open+project:DonkeyCoyote/proprietary_vendor_sony" -g https://review.choose-a.name -P vendor/sony
 
-echo "-----Patching done --------------"
+echo -e ${GREEN}"-----Patching done --------------"${NC}

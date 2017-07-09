c05a5a6733e901fce98f60fa6a05a71c4c4abc3e
nailyk-fr <nailyk_git@nailyk.fr>
2017-07-09 18:58:12 +0200

    Releases: Use lineage name
    Change-Id: I9366a5da07bc611150eadb9c427b29a084015b39

--- release.sh
+++ release.sh
@@ -8,7 +8,7 @@ if [ ! -d "$HOME/../releases/cm-12.1" ]; then
 fi
 
 if [ -d "$HOME/../releases/cm-12.1" ]; then 
-	cp -n $HOME/out/target/product/$1/cm-*.zip* $HOME/../releases/cm-12.1
+	cp -n $HOME/out/target/product/$1/lineage-*.zip* $HOME/../releases/cm-12.1
 else
 	false
 fi

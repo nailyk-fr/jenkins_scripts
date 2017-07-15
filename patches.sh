5b8a9bafd1eaf699b5158fd5f4e12f115a451cd4
nailyk-fr <nailyk_git@nailyk.fr>
2017-07-15 19:43:29 +0200

    Patchs: Update exlucde list
    Change-Id: I9e0c0d24e1128b60345ba58b8587c02fd9eed96e

--- patches.sh
+++ patches.sh
@@ -31,16 +31,16 @@ echo -e ${YELLOW}"-----Patching $PWD"${NC}
 git fetch github cm-14.1
 git checkout github/cm-14.1
 cd ~/
-repopick -Q "status:open+branch:cm-14.1+project:LineageOS/android_device_sony_msm8974-common" -P device/sony/msm8974-common --exclude "179539"
+repopick -Q "status:open+branch:cm-14.1+project:LineageOS/android_device_sony_msm8974-common" -P device/sony/msm8974-common --exclude "179539,179539"
 
 cd ~/device/sony/shinano-common
 echo -e ${YELLOW}"-----Patching $PWD"${NC}
 git fetch github cm-14.1
 git checkout github/cm-14.1
 cd ~/
-repopick -Q "status:open+branch:cm-14.1+project:LineageOS/android_device_sony_shinano-common" -P device/sony/shinano-common --exclude "179444,179443"
+repopick -Q "status:open+branch:cm-14.1+project:LineageOS/android_device_sony_shinano-common" -P device/sony/shinano-common --exclude "179443"
 cd ~/device/sony/shinano-common
-git fetch ssh://nailyk@review.lineageos.org:29418/LineageOS/android_device_sony_shinano-common refs/changes/64/181164/2 && git cherry-pick FETCH_HEAD
+git fetch ssh://nailyk@review.lineageos.org:29418/LineageOS/android_device_sony_shinano-common refs/changes/64/181164/3 && git cherry-pick FETCH_HEAD
 
 # Need to be pick manually, always merge conflicts
 #repopick -g https://gerrit.nailyk.fr -P device/sony/shinano-common 196

378744f8c7928dd795262879edb9827c5be70f15
nailyk-fr <nailyk_git@nailyk.fr>
2017-07-15 20:59:16 +0200

    Patchs: Add patch to remove time_daemon
    Change-Id: Ibacd0f3d88ced52568f8fa99f9a028a071c01202

--- patches.sh
+++ patches.sh
@@ -63,6 +63,7 @@ git fetch choosea android-7.1
 git checkout choosea/android-7.1
 cd ~/
 $MYFOLDER/repopick.py -Q "status:open+project:DonkeyCoyote/proprietary_vendor_sony" -g https://review.choose-a.name -P vendor/sony
+$MYFOLDER/repopick.py -g https://review.choose-a.name -P vendor/sony 200 -f
 
 cd ~/hardware/sony/timekeep
 echo -e ${YELLOW}"-----Patching $PWD"${NC}

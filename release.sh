02743e40fce9e49a8cb0159b0a124a249d7457af
nailyk-fr <nailyk_git@nailyk.fr>
2017-07-30 19:32:51 +0200

    Releases: Add colors and md5 for twrp
    Change-Id: I55b508ddba198e947f9fffc61201306ba178f3f0

--- release.sh
+++ release.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+MYFOLDER="$(dirname "$(realpath "$0")")"
+source $MYFOLDER/colors.sh
 
 if [ ! -d "$HOME/../releases/cm-14.1" ]; then 
 	mkdir -p $HOME/../releases/cm-14.1
 fi
 
 if [ -d "$HOME/../releases/cm-14.1" ]; then 
-	echo copy lineage
-	find $HOME/out/target/product -maxdepth 2 -mindepth 2 -iname "lineage-*.zip*" -exec cp --verbose --update {} $HOME/../releases/cm-14.1 \;
+	echo -e ${BLUE}"copy lineage"${NC}
+	find $HOME/out/target/product -maxdepth 2 -mindepth 2 -iname "lineage-*.zip*" \
+    -exec echo -e ${GREEN}"copying {}"${NC} \; \
+    -exec cp --verbose --update {} $HOME/../releases/cm-14.1 \;
 else
 	false
 fi
@@ -18,13 +22,17 @@ fi
 
 if [ -d "$HOME/../releases/twrp" ]; then
 	for folder in `find $HOME/out/target/product -maxdepth 1 -type d`; do
-		echo copy twrp
+		echo -e ${BLUE}"copy twrp from $folder"${NC}
 		filename="twrp_$(basename $folder)_$(date +"%Y-%m-%d")"
 		if [ -e "$folder/recovery.img" ]; then
+			echo -e ${GREEN}"    Copying $filename.img"${NC}
 			eval cp --verbose --update $folder/recovery.img $HOME/../releases/twrp/$filename.img
+			echo "addDescription \"md5: $(md5sum $HOME/../releases/twrp/$filename.img | awk '{ print $1 }')\" $filename.img" >> $HOME/../releases/twrp/.htaccess
 		fi
 		if [ -e "$folder/recovery-updater-signed.zip" ]; then
+			echo -e ${GREEN}"    Copying $filename.img"${NC}
 			eval cp --verbose --update $folder/recovery-updater-signed.zip $HOME/../releases/twrp/$filename.zip
+			echo "addDescription \"md5: $(md5sum $HOME/../releases/twrp/$filename.img | awk '{ print $1 }')\" $filename.img" >> $HOME/../releases/twrp/.htaccess
 		fi
 	done
 else

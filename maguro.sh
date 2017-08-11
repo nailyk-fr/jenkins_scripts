362c7d6625ec67d3f60da470a56a00bf9246a4dd
nailyk <nailyk-fr@users.noreply.github.com>
2017-08-11 15:10:11 +0200

    Maguro: Cleanup build script

--- maguro.sh
+++ maguro.sh
@@ -22,14 +22,6 @@ echo "-----Patching $PWD"
 git reset --hard
 git clean -fxd :/
 git checkout github/cm-13.0
-git fetch https://review.lineageos.org/LineageOS/android_hardware_ti_omap4 refs/changes/12/163412/1 && git cherry-pick FETCH_HEAD
-git fetch https://review.lineageos.org/LineageOS/android_hardware_ti_omap4 refs/changes/13/163413/1 && git cherry-pick FETCH_HEAD
-git fetch https://review.lineageos.org/LineageOS/android_hardware_ti_omap4 refs/changes/14/163414/1 && git cherry-pick FETCH_HEAD
-git fetch https://review.lineageos.org/LineageOS/android_hardware_ti_omap4 refs/changes/15/163415/1 && git cherry-pick FETCH_HEAD
-git fetch https://review.lineageos.org/LineageOS/android_hardware_ti_omap4 refs/changes/16/163416/1 && git cherry-pick FETCH_HEAD
-git fetch https://review.lineageos.org/LineageOS/android_hardware_ti_omap4 refs/changes/17/163417/1 && git cherry-pick FETCH_HEAD
-git fetch https://review.lineageos.org/LineageOS/android_hardware_ti_omap4 refs/changes/20/163420/1 && git cherry-pick FETCH_HEAD
-
 
 cd ~/
 export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4g"
@@ -41,7 +33,7 @@ echo "--------jack args $ANDROID_JACK_VM_ARGS"
 echo "-----Trigger build from $PWD"
 source ./build/envsetup.sh
 breakfast maguro
-make bacon -j6
+mka bacon
 
 #kill jack when done to prevent error on other builds
 ./prebuilts/sdk/tools/jack-admin kill-server

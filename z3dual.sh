1c3c2f03b38a231edf672124af46ccb492e09d12
nailyk-fr <nailyk_git@nailyk.fr>
2017-08-22 19:20:43 +0200

    Build: Remove double patching
    Change-Id: I176ce1077a620e3a79d076543d74ef52ea93d816

--- z3dual.sh
+++ z3dual.sh
@@ -7,7 +7,7 @@ export WITH_SU=true
 MYFOLDER=$PWD/scripts
 
 echo "---------patching"
-./${MYFOLDER}/patches.sh
+
 
 cd ~/
 export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx8g"

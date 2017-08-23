ecf61a7c4c0ea44b351dc641a81a94b6b1ce8620
stay steadfast <steadfasterX@users.noreply.github.com>
2017-08-23 14:14:23 +0200

    replaced dups by function
    instead of duplicating every step we use 1 single function for all.
    besides that you just need to add a new path in the DEVPATHS variable
    when required.

--- merge.sh
+++ merge.sh
@@ -6,42 +6,21 @@ source $MYFOLDER/colors.sh
 echo -e ${GREEN}"---------home set to: $HOME"${NC}
 echo -e ${GREEN}"Lets merge LineageOS"${NC}
 
-cd ~/device/sony/z3dual
-echo -e ${YELLOW}"-----Merging $PWD"${NC}
-git fetch gerrit cm-14.1_twrp
-git fetch lineage cm-14.1
-git checkout gerrit/cm-14.1_twrp
-git merge lineage/cm-14.1 --no-edit
-git push gerrit HEAD:cm-14.1_twrp
+# space separated list of device paths
+DEVPATHS="~/device/sony/z3dual ~/device/sony/z3 ~/device/sony/z3c ~/device/sony/shinano-common ~/device/sony/msm8974-common"
 
-cd ~/device/sony/z3
-echo -e ${YELLOW}"-----Merging $PWD"${NC}
-git fetch gerrit cm-14.1_twrp
-git fetch lineage cm-14.1
-git checkout gerrit/cm-14.1_twrp
-git merge lineage/cm-14.1 --no-edit
-git push gerrit HEAD:cm-14.1_twrp
+# merge
+F_MERGE(){
+  cd $1
+  echo -e ${YELLOW}"-----Merging $PWD"${NC}
+  git fetch gerrit cm-14.1_twrp
+  git fetch lineage cm-14.1
+  git checkout gerrit/cm-14.1_twrp
+  git merge lineage/cm-14.1 --no-edit
+  git push gerrit HEAD:cm-14.1_twrp  
+}
 
-cd ~/device/sony/z3c
-echo -e ${YELLOW}"-----Merging $PWD"${NC}
-git fetch gerrit cm-14.1_twrp
-git fetch lineage cm-14.1
-git checkout gerrit/cm-14.1_twrp
-git merge lineage/cm-14.1 --no-edit
-git push gerrit HEAD:cm-14.1_twrp
-
-cd ~/device/sony/shinano-common
-echo -e ${YELLOW}"-----Merging $PWD"${NC}
-git fetch gerrit cm-14.1
-git fetch lineage cm-14.1
-git checkout gerrit/cm-14.1
-git merge lineage/cm-14.1 --no-edit
-git push gerrit HEAD:cm-14.1
-
-cd ~/device/sony/msm8974-common
-echo -e ${YELLOW}"-----Merging $PWD"${NC}
-git fetch gerrit cm-14.1
-git fetch lineage cm-14.1
-git checkout gerrit/cm-14.1
-git merge lineage/cm-14.1 --no-edit
-git push gerrit HEAD:cm-14.1
+# do a merge for any defined path
+for merge in $DEVPATHS;do
+  F_MERGE "$merge"
+done

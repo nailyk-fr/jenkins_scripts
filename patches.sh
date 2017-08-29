ec46cdb6db14d9b0e75d5ba601ec3799889a279a
nailyk-fr <nailyk_git@nailyk.fr>
2017-08-29 13:33:23 +0200

    Patchs: Include power.leo remove patch

--- patches.sh
+++ patches.sh
@@ -57,7 +57,7 @@ cd ~/device/sony/leo
 git fetch choose android-7.1
 git checkout choose/android-7.1
 cd ~/
-$MYFOLDER/repopick.py -Q "status:open+project:android_device_sony_leo"  -g https://review.choose-a.name -P device/sony/leo --exclude "817"
+$MYFOLDER/repopick.py -Q "status:open+project:android_device_sony_leo"  -g https://review.choose-a.name -P device/sony/leo
 
 echo -e ${YELLOW}"-----Patching kernel/sony/msm8974"${NC}
 cd ~/kernel/sony/msm8974

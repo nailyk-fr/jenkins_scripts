5c2a37204e4c1b45dbdfbb2e95c9b821b2905b2d
nailyk-fr <jenkins@nailyk.fr>
2017-09-18 14:37:05 +0200

    Build: Unify build scripts
    Change-Id: If7b56c03e0c6a662b7e563460cae738cf73a25a4

--- build.sh
+++ build.sh
@@ -6,17 +6,23 @@ export USE_NINJA=false
 export WITH_SU=true
 MYFOLDER=$PWD/scripts
 
+source $MYFOLDER/colors.sh
+
 cd ~/
 export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4g"
 export ANDROID_JACK_VM_ARGS="$JACK_SERVER_VM_ARGUMENTS"
 
 ./prebuilts/sdk/tools/jack-admin kill-server || true
 
-echo "--------jack args $ANDROID_JACK_VM_ARGS"
-echo "-----Trigger build from $PWD"
+echo -e ${GREEN}"--------jack args $ANDROID_JACK_VM_ARGS"${NC}
 source ./build/envsetup.sh
-breakfast leodual
-mka bootimage
+breakfast $CODENAME
+echo -e ${BLUE}"Gonna build $TARGET for $CODENAME"${NC}
+mka $TARGET
+RET="$?"
 
 #kill jack when done to prevent error on other builds
 ./prebuilts/sdk/tools/jack-admin kill-server || true
+
+exit $RET
+

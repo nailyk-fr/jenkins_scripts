32917ccbb3772ca8b98d0671326b3dbd0e95ff8f
nailyk-fr <jenkins@nailyk.fr>
2017-09-18 12:50:18 +0200

    Patchs: More workaround for omni.dependencies
    Change-Id: I949bd1234167c171cdc2218c2aaf0d9a6ac4ff51

--- patches.sh
+++ patches.sh
@@ -32,8 +32,11 @@ git checkout choose/android-8.0
 cd ~/
 $MYFOLDER/repopick.py -g https://review.choose-a.name -P device/sony/msm8974-common 913
 $MYFOLDER/repopick.py -g https://review.choose-a.name -P device/sony/msm8974-common 914
+$MYFOLDER/repopick.py -g https://review.choose-a.name -P device/sony/msm8974-common 1039
+$MYFOLDER/repopick.py -g https://review.choose-a.name -P device/sony/msm8974-common 1040
+$MYFOLDER/repopick.py -g https://review.choose-a.name -P device/sony/msm8974-common 1041
 echo -e ${RED}"klsjdfhksdhf"${NC}
-$MYFOLDER/repopick.py -Q "status:open+project:android_device_sony_msm8974-common+branch:android-8.0"  -g https://review.choose-a.name -P device/sony/msm8974-common
+$MYFOLDER/repopick.py -Q "status:open+project:android_device_sony_msm8974-common+branch:android-8.0"  -g https://review.choose-a.name -P device/sony/msm8974-common --exclude "913,914,1039,1040,1041"
 
 cd ~/device/sony/shinano-common
 echo -e ${YELLOW}"-----Patching $PWD"${NC}

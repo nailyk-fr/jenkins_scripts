4f6b1be1407abc46a59934602ae19b0b35f6e0b4
nailyk-fr <jenkins@nailyk.fr>
2017-09-18 15:23:34 +0200

    Patchs: Pick aries vendor changes
    Change-Id: I73efeb160fc9e871c37f8265aa42fe8f7ea201dd

--- patches.sh
+++ patches.sh
@@ -107,7 +107,7 @@ git fetch github android-7.1
 git checkout github/android-7.1
 cd ~/
 #$MYFOLDER/repopick.py -Q "status:open+project:DonkeyCoyote/proprietary_vendor_sony" -g https://review.choose-a.name -P vendor/sony
-$MYFOLDER/repopick.py -g https://review.choose-a.name -P vendor/sony 757
+$MYFOLDER/repopick.py -g https://review.choose-a.name -P vendor/sony 1019
 
 cd ~/hardware/sony/timekeep
 echo -e ${YELLOW}"-----Patching $PWD"${NC}

9d6543ee578dc72c4e66c21c537ca4fedc035db0
nailyk-fr <nailyk_git@nailyk.fr>
2017-11-03 09:25:31 +0100

    Patchs: Remove "Add missing flags"

--- patches.sh
+++ patches.sh
@@ -59,7 +59,7 @@ git fetch omnirom android-8.0
 git checkout omnirom/android-8.0
 cd ~/
 echo -e ${RED}"full pick"${NC}
-$MYFOLDER/repopick.py -Q "NOT+label:Code-Review=-2+AND+NOT+label:Verified=-1+(status:open+project:android_device_sony_msm8974-common+branch:android-8.0)"  -g https://gerrit.omnirom.org -P device/sony/msm8974-common
+$MYFOLDER/repopick.py -Q "NOT+label:Code-Review=-2+AND+NOT+label:Verified=-1+(status:open+project:android_device_sony_msm8974-common+branch:android-8.0)"  -g https://gerrit.omnirom.org -P device/sony/msm8974-common --exclude="26557"
 
 cd ~/device/sony/shinano-common
 echo -e ${YELLOW}"-----Patching $PWD"${NC}

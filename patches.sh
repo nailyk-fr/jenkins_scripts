ee62bc70e378c349af1be06592607188e9e42fc6
nailyk-fr <nailyk_git@nailyk.fr>
2017-11-27 13:56:41 +0100

    Patchs: Add pendign GPS changes

--- patches.sh
+++ patches.sh
@@ -54,6 +54,9 @@ git checkout omnirom/android-8.0
 cd ~/
 echo -e ${RED}"full pick"${NC}
 $MYFOLDER/repopick.py -Q "NOT+label:Code-Review=-2+AND+NOT+label:Verified=-1+(status:open+project:android_device_sony_msm8974-common+branch:android-8.0)"  -g https://gerrit.omnirom.org -P device/sony/msm8974-common --exclude="26557"
+cd ~/device/sony/msm8974-common
+git fetch ssh://nailyk@gerrit.omnirom.org:29418/android_device_sony_msm8974-common refs/changes/76/27076/2 && git cherry-pick FETCH_HEAD
+git fetch ssh://nailyk@gerrit.omnirom.org:29418/android_device_sony_msm8974-common refs/changes/77/27077/1 && git cherry-pick FETCH_HEAD
 
 cd ~/device/sony/shinano-common
 echo -e ${YELLOW}"-----Patching $PWD"${NC}
@@ -117,8 +120,10 @@ cd ~/vendor/sony
 git fetch github android-8.0
 git fetch nailyk android-8.0
 git checkout nailyk/android-8.0
-$MYFOLDER/repopick.py -Q "NOT+label:Code-Review=-2+AND+NOT+label:Verified=-1+(status:open+project:omni_vendor_sony+branch:android-8.0)"  -g https://gerrit.nailyk.fr -P vendor/sony
 cd ~/
+$MYFOLDER/repopick.py -Q "NOT+label:Code-Review=-2+AND+NOT+label:Verified=-1+(status:open+project:omni_vendor_sony+branch:android-8.0)"  -g https://gerrit.nailyk.fr -P vendor/sony
+cd ~/vendor/sony
+git fetch http://gerrit.nailyk.fr/omni_vendor_sony refs/changes/79/1079/1 && git cherry-pick FETCH_HEAD
 
 cd ~/hardware/sony/timekeep
 echo -e ${YELLOW}"-----Patching $PWD"${NC}

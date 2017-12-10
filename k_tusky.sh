344e5fc5d7a6312d5aa626e6e5f4f467572c13f6
nailyk-fr <nailyk_git@nailyk.fr>
2017-12-10 20:26:07 +0100

    Tusky: update output apk path
    Change-Id: I5e11684fa4e6245f4fe76bee630ffcae725fdadf
    Signed-off-by: nailyk-fr <nailyk_git@nailyk.fr>

--- k_tusky.sh
+++ k_tusky.sh
@@ -17,4 +17,4 @@ APP_BUILD_SCRIPT=$PWD/app/src/main/jni/Android.mk \
 ./gradlew assembleRelease
 ./gradlew --stop || true
 killall aapt || true
-cp app/build/outputs/apk/app-fdroid-release.apk ~/fdroiddata/repo/Tusky.apk
+cp app/build/outputs/apk/fdroid/release/app-fdroid-release.apk ~/fdroiddata/repo/Tusky.apk

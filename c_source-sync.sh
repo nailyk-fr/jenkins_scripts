368bde2fe608f54373e052e6ce50e987b0a67bda
nailyk-fr <nailyk_git@nailyk.fr>
2017-12-10 20:18:53 +0100

    OpenLauncher: Initial commit
    Change-Id: I43123ab2256067f57a6a86484b246d8b27f13a47
    Signed-off-by: nailyk-fr <nailyk_git@nailyk.fr>

--- c_source-sync.sh
+++ c_source-sync.sh
@@ -136,6 +136,32 @@ android {
 }" >> app/build.gradle
 
 
+cd ~/apps/OpenLauncher/
+git reset --hard
+git clean -fxd :/
+git fetch origin master
+git checkout origin/master
+
+wget "https://raw.githubusercontent.com/nailyk-fr/jenkins_scripts/nougat-mr1.1-release/patches/AndroidVTS.properties" -O local.properties
+cat "/var/jenkins_home/workspace/F-Droid/.signing" >> app/gradle.properties
+echo "
+android {
+    signingConfigs {
+       release {
+           storeFile file(RELEASE_STORE_FILE)
+           storePassword RELEASE_STORE_PASSWORD
+           keyAlias RELEASE_KEY_ALIAS
+           keyPassword RELEASE_KEY_PASSWORD
+       }
+    }
+
+    buildTypes {
+            release {
+                signingConfig signingConfigs.release
+            }
+    }
+}" >> app/build.gradle
+
 cd ~/apps/vlc-android/
 git reset --hard
 git clean -fxd :/
---
+++ k_openlauncher.sh
@@ -0,0 +1,20 @@
+export USER=jenkins
+export ANDROID_HOME=/var/jenkins_home/SDK
+export GRADLE_USER_HOME=$HOME
+export NDK_PROJECT_PATH=/var/jenkins_home/NDK/android-ndk-r13b
+
+export LANG=C
+unset _JAVA_OPTIONS
+export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4g"
+export ANDROID_JACK_VM_ARGS="$JACK_SERVER_VM_ARGUMENTS"
+
+
+cd ~/apps/OpenLauncher/
+
+APP_PROJECT_PATH=$PWD/app/src/main \
+NDK_PROJECT_PATH=$PWD/app/src/main \
+APP_BUILD_SCRIPT=$PWD/app/src/main/jni/Android.mk \
+./gradlew assembleRelease
+./gradlew --stop || true
+killall aapt || true
+cp app/build/outputs/apk/flavorDefault/release/app-flavorDefault-release.apk ~/fdroiddata/repo/OpenLauncher.apk

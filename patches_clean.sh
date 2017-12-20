a1c42f0776c235c5469dfa2a8955ba642c9a1de6
nailyk-fr <nailyk_git@nailyk.fr>
2017-12-20 21:31:09 +0100

    Patches_clean: Remove omap4 kernel
    Signed-off-by: nailyk-fr <nailyk_git@nailyk.fr>

--- patches_clean.sh
+++ patches_clean.sh
@@ -184,11 +184,5 @@ git reset --hard
 git clean -fxd :/
 git checkout omnirom/android-8.1
 
-cd ~/kernel/ti/omap4
-echo -e ${YELLOW}"-----Cleaning $PWD"${NC}
-git reset --hard
-git clean -fxd :/
-git checkout omnirom/android-8.1
-
 cd ~/
 

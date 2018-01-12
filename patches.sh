57424ee46ac635b33a00b29f9b6f533dce798dc8
nailyk-fr <nailyk_git@nailyk.fr>
2018-01-12 19:26:26 +0100

    Patches: Test commit to disable messanging app
    Signed-off-by: nailyk-fr <nailyk_git@nailyk.fr>

--- patches.sh
+++ patches.sh
@@ -21,6 +21,7 @@ repopick 27694 # twrp: Remove APP
 repopick 27890 # add omni/vendor/build/core/certs.mk file
 repopick 27891 # add omni/vendor/build/core/certs.mk include
 repopick 28155 # sony/location Guard makefiles
+repopick 28275 # Allow messaging app disable
 
 cd ~/frameworks/av
 echo -e ${YELLOW}"-----Patching $PWD"${NC}

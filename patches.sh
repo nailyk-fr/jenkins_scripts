6464b25448c5f6ba1ac865e9c38e1f2bce7f6719
nailyk-fr <nailyk_git@nailyk.fr>
2018-01-22 21:15:10 +0100

    Patches: Exclude audio hal changes for now
    Signed-off-by: nailyk-fr <nailyk_git@nailyk.fr>

--- patches.sh
+++ patches.sh
@@ -75,7 +75,7 @@ git clean -fxd :/
 git fetch omnirom android-8.1
 git checkout omnirom/android-8.1
 cd ~/
-$MYFOLDER/repopick.py -Q "NOT+label:Code-Review=-2+AND+NOT+label:Verified=-1+(status:open+project:android_hardware_qcom_audio-caf-msm8974+branch:android-8.1)" -g https://gerrit.omnirom.org -P hardware/qcom/audio-caf-msm8974 --exclude "27937"
+#$MYFOLDER/repopick.py -Q "NOT+label:Code-Review=-2+AND+NOT+label:Verified=-1+(status:open+project:android_hardware_qcom_audio-caf-msm8974+branch:android-8.1)" -g https://gerrit.omnirom.org -P hardware/qcom/audio-caf-msm8974 --exclude "27937"
 
 cd ~/device/sony/msm8974-common
 echo -e ${YELLOW}"-----Patching $PWD"${NC}

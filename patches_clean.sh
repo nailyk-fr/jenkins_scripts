4dd8c8943027dd73eda932c83a4dbf73763ba315
nailyk-fr <nailyk_git@nailyk.fr>
2018-01-26 16:28:38 +0100

    Clean: Add vold
    Signed-off-by: nailyk-fr <nailyk_git@nailyk.fr>

--- patches_clean.sh
+++ patches_clean.sh
@@ -28,6 +28,13 @@ git reset --hard
 git clean -fxd :/
 git checkout omnirom/android-8.1
 
+cd ~/system/vold
+echo -e ${YELLOW}"-----Patching $PWD"${NC}
+git reset --hard
+git fetch omnirom android-8.1
+git checkout omnirom/android-8.1
+cd ~/
+
 cd ~/packages/apps/OpenDelta
 echo -e ${YELLOW}"-----Cleaning $PWD"${NC}
 git reset --hard

12316bf82184abdac0f3aeaca59191ee3491959a
nailyk <nailyk-fr@users.noreply.github.com>
2018-03-18 20:54:33 +0100

    OpenLauncher: Verbose copy apk
     * Ensure copy apk result will be shown in build log

--- k_openlauncher.sh
+++ k_openlauncher.sh
@@ -17,4 +17,4 @@ APP_BUILD_SCRIPT=$PWD/app/src/main/jni/Android.mk \
 ./gradlew assembleRelease
 ./gradlew --stop || true
 killall aapt || true
-cp app/build/outputs/apk/flavorDefault/release/app-flavorDefault-release.apk ~/fdroiddata/repo/OpenLauncher.apk
+cp -v app/build/outputs/apk/flavorDefault/release/app-flavorDefault-release.apk ~/fdroiddata/repo/OpenLauncher.apk

3cb8b41a27fd5a60f2efb825a2be353659a85291
nailyk-fr <nailyk_git@nailyk.fr>
2019-03-16 18:50:58 +0100

    config: Correct typo
    Signed-off-by: nailyk-fr <nailyk_git@nailyk.fr>

--- config.sh
+++ config.sh
@@ -4,5 +4,5 @@ PICKOFFICIAL="nailyk"
 PICKCUSTOM="nailyk-fr"
 
 git config --global user.email "jenkins@nailyk.fr"
-git config --global user.name ${PICKNAILYK}
+git config --global user.name "${PICKCUSTOM}"
 

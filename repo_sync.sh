c8bb06f18f6d51d949f0c150ce24bab9faafc19f
nailyk <jenkins@nailyk.fr>
2019-05-05 20:17:21 +0200

    sync: Add fancy
    Change-Id: I6569592e87d9c22124b0e5b25984e5d90590c438
    Signed-off-by: nailyk <jenkins@nailyk.fr>

--- repo_sync.sh
+++ repo_sync.sh
@@ -1,6 +1,8 @@
 BUILD_VER="android-9.0"
 LOCAL_BRANCH="${BUILD_VER}"
 
+echo -e ${GREEN}".o0o. .o0o..o0o..o0o. syncing repos .o0o. .o0o..o0o..o0o."${NC}
+
 cd ~/.repo
 if [ ! -d "$HOME/.repo/local_manifests" ]; then
   git clone https://github.com/nailyk-fr/local_manifests.git -b ${LOCAL_BRANCH} local_manifests

541325c70b580f6dd02701ca7b4d9286e1b05124
nailyk-fr <jenkins@nailyk.fr>
2019-08-17 14:33:21 +0200

    sync: Do not override local_manifest
    Change-Id: I294ceaf1b1f66e3907c9a3f5cee1e653ae538782
    Signed-off-by: nailyk-fr <jenkins@nailyk.fr>

--- repo_sync.sh
+++ repo_sync.sh
@@ -3,15 +3,6 @@ LOCAL_BRANCH="${BUILD_VER}"
 
 echo -e ${GREEN}".o0o. .o0o..o0o..o0o. syncing repos .o0o. .o0o..o0o..o0o."${NC}
 
-cd ~/.repo
-if [ ! -d "$HOME/.repo/local_manifests" ]; then
-  git clone https://github.com/nailyk-fr/local_manifests.git -b ${LOCAL_BRANCH} local_manifests
-else
-  cd ~/.repo/local_manifests
-  git fetch origin ${LOCAL_BRANCH}
-  git checkout "origin/${LOCAL_BRANCH}"
-fi
-
 cd ~
 repo sync --force-sync -j 8
 echo "--------- repo sync end"

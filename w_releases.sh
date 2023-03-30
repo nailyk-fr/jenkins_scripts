dd7e99283f797eabfa1c8ee87d437a4740d5f8bc
nailyk <nailyk-fr@users.noreply.github.com>
2023-03-30 08:46:45 +0200

    w_releases: Update env

--- w_releases.sh
+++ w_releases.sh
@@ -1,10 +1,22 @@
 export HOME=${WORKSPACE}
 echo "--------- home set"
 
-export USER=jenkins
+echo ${GREEN}"--------------------- preparing env "${NC}
+
+MYFOLDER="$(dirname "$(realpath "$0")")"
+. $MYFOLDER/colors.sh
 
 export PATH=/var/jenkins_home/workspace/F-Droid/fdroidserver:$PATH
-cd ~/fdroidserver
+
+export USER=jenkins
+export ANDROID_HOME=/var/jenkins_home/SDK
+export GRADLE_USER_HOME=$HOME
+export NDK_PROJECT_PATH=/var/jenkins_home/NDK/android-ndk-r13b
+
+export LANG=C.UTF-8
+unset _JAVA_OPTIONS
+export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4g"
+export ANDROID_JACK_VM_ARGS="$JACK_SERVER_VM_ARGUMENTS"
 
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8

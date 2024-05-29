ff58274944e280362192f16fd9d9a0b08fd2e7fe
nailyk-fr <nailyk_git@nailyk.fr>
2024-05-29 15:29:26 +0200

    a_github_tusky: add git config username & email
    Signed-off-by: nailyk-fr <nailyk_git@nailyk.fr>

--- a_github_tusky.sh
+++ a_github_tusky.sh
@@ -26,6 +26,10 @@ echo ${GREEN}"--------------------- patching sources "${NC}
 # Inside the auto triggered script. So workspace is the root folder of the git clone
 cd ${WORKSPACE}
 
+## configure git to allow cherry-pick
+git config --global user.email "jenkins_git@nailyk.fr"
+git config --global user.name "nailyk"
+
 # cherry pick commit to change version number
 git cherry-pick 230f8f3917e4e6fe9f376293c4727bfbea7cf1a4
 

73c5a8f464dd6102be4d07ddfab21d666f011f27
nailyk-fr <nailyk_git@nailyk.fr>
2026-05-25 20:24:43 +0100

    k_Signal_downloader: Update path for f-droid repo
    Signed-off-by: nailyk-fr <nailyk_git@nailyk.fr>

--- k_Signal_downloader.sh
+++ k_Signal_downloader.sh
@@ -14,39 +14,39 @@ MYFOLDER="$(dirname "$(realpath "$0")")"
 
 export PATH=/var/jenkins_home/workspace/F-Droid/fdroidserver:$PATH
 
-echo ${GREEN}"--------------------- patching sources "${NC}
+echo ${GREEN}"--------------------- Downloading "${NC}
 
 # Inside the auto triggered script. So workspace is the root folder of the git clone
 cd ${WORKSPACE}
 
+G_FDROID_FOLDER="~/fdroiddata/repo"
+
 ##### End of - All the bad Jenkins hack I made
-
-
-
 
 T_SIGNAL_LATEST_JSON=$(curl -s https://api.github.com/repos/signalapp/Signal-Android/releases/latest)
 
 T_LATEST_FILENAME=$(echo "${T_SIGNAL_LATEST_JSON}" | jq '.assets[].name' | tr -d '"')
+T_FULL_PATH="${G_FDROID_FOLDER}/${T_LATEST_FILENAME}"
 T_LATEST_URL=$(echo "${T_SIGNAL_LATEST_JSON}" | jq '.assets[].browser_download_url' | tr -d '"')
 T_DIGEST=$(echo "${T_SIGNAL_LATEST_JSON}" | jq '.assets[].digest' | tr -d '"' | cut -d ':' -f2)
 
 
 
-echo "Latest Filename: ${T_LATEST_FILENAME}"
+echo "Latest Filename: ${T_LATEST_FILENAME} -- ${T_FULL_PATH}"
 echo "Latest URL : ${T_LATEST_URL}"
 
-# if [[ ! -f "./${T_LATEST_FILENAME}" ]]; then
-  echo "File does not exist. Downloading"
-  wget --continue "${T_LATEST_URL}" -O "${T_LATEST_FILENAME}"
-# fi
+wget --continue "${T_LATEST_URL}" -O "${T_FULL_PATH}"
 
-T_COMPUTE_DIGEST=$(sha256sum "./${T_LATEST_FILENAME}" | awk '{print $1}')
+echo ${GREEN}"--------------------- Digesting "${NC}
+
+
+T_COMPUTE_DIGEST=$(sha256sum "${T_FULL_PATH}" | awk '{print $1}')
 
 echo "Retrieved digest : ${T_DIGEST}"
 echo "Computed digest : ${T_COMPUTE_DIGEST}"
 
 if [[ "${T_DIGEST}" != "${T_COMPUTE_DIGEST}" ]]; then
   echo "There was an issue downloading the file. Removing..."
-  rm -v "${T_LATEST_FILENAME}"
+  rm -v "${T_FULL_PATH}"
   exit 1 # Exit with error code to mark the build status to fail in Jenkins
 fi
